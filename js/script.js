ee8af3c6fb76c1a69cc2c838e6b638e5d5e91941
Agent2018 <105843136+Agent2018@users.noreply.github.com>
2022-05-19 11:14:07 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 3,
+    });
+});
+
+$(document).ready(function(){
+    $('.slider-two').slick({
+        slidesToShow: 4,
+    });
+});
